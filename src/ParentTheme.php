205a6a8a161b96d3ac9a2d84f1bce6416364dfac
saasfreelancer <syed.sayal.shah@gmail.com>
2024-11-29 18:13:19 +0500

    refactor: add $inherit parameter to support child theme asset fallback

--- src/ParentTheme.php
+++ src/ParentTheme.php
@@ -17,9 +17,15 @@ class ParentTheme extends AssetsAbstract {
      * Get the filesystem path for a file within the parent theme.
      *
      * @param string $file File path within the parent theme.
+     * @param bool   $inherit Whether to check for the file in the child theme first.
+     *                        Set to `true` to check the child theme first, `false` to use the parent theme only.
      * @return string Absolute filesystem path of the file.
      */
-    public function path( $file = '' ): string {
+    public function path( $file = '', $inherit = false ): string {
+        if ( $inherit ) {
+            return get_theme_file_path( $file );
+        }
+
         return get_parent_theme_file_path( $file );
     }
 
@@ -27,9 +33,15 @@ class ParentTheme extends AssetsAbstract {
      * Get the URL for a file within the parent theme.
      *
      * @param string $file File path within the parent theme.
+     * @param bool   $inherit Whether to check for the file in the child theme first.
+     *                         Set to `true` to check the child theme first, `false` to use the parent theme only.
      * @return string URL of the file.
      */
-    public function url( $file = '' ): string {
+    public function url( $file = '', $inherit = false ): string {
+        if ( $inherit ) {
+            return get_theme_file_uri( $file );
+        }
+
         return get_parent_theme_file_uri( $file );
     }
 
